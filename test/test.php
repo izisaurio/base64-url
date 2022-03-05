b4d6dfcdf0bff6b5f0f2191d1714fe6b8bc46903
izisaurio <izi.isaac@gmail.com>
2022-03-05 15:13:47 -0600

    First commit

---
+++ test/test.php
@@ -0,0 +1,13 @@
+<?php
+
+require "../vendor/autoload.php";
+
+use Inn\Base64Url;
+
+$encoded = Base64Url::encode("izisaurio");
+
+var_dump($encoded);
+
+$decoded = Base64Url::decode($encoded);
+
+var_dump($decoded);
